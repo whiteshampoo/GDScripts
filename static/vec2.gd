6997291c6af19e9c91bf39560180ecd3d89dcd17
Benedikt Wicklein <benedikt.wicklein@gmail.com>
2025-02-14 23:08:20 +0100

    Vec2.swap and documentation fixes

--- static/vec2.gd
+++ static/vec2.gd
@@ -1,6 +1,10 @@
 class_name Vec2
 ## Useful methods to do stuff with Vector2
 
+## Swaps the x- and y-value
+static func swap(vector: Vector2) -> Vector2:
+	return Vector2(vector.y, vector.x)
+
 
 ## Calculates the mean between the x- and y-value.
 ## [codeblock lang=gdscript]Vec2.value_mean(Vector2(0.5, 1.0)) # -> 0.75[/codeblock]
@@ -11,12 +15,14 @@ static func value_mean(vector: Vector2) -> float:
 ## Calculates the lerp between the x- and y-value by weight.
 ## [codeblock lang=gdscript]Vec2.value_lerp(Vector2(-1.0, 1.0), 0.75) # -> 0.5[/codeblock]
 static func value_lerp(vector: Vector2, weight) -> float:
-	return lerp(vector.x, vector.y, weight)
+	return lerpf(vector.x, vector.y, weight)
 
-## Directly converts a Vector2 to Vector3.
+
+## Directly converts a [Vector2] to [Vector3].
 static func to_vec3_xy(vector: Vector2, z: float = 0.0) -> Vector3:
 	return Vector3(vector.x, vector.y, z)
 
-## Converts a Vector2 to Vector3 where the y-value becomes the z-value.
+
+## Converts a [Vector2] to [Vector3] where the y-value becomes the z-value.
 static func to_vec3_xz(vector: Vector2, y: float = 0.0) -> Vector3:
 	return Vector3(vector.x, y, vector.y)
