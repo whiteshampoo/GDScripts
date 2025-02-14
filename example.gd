93b3345b7d5fc28a66e3a1ae5e1d178e17dafdde
Substain <Dav.d@gmx.de>
2025-02-14 23:58:10 +0100

    Update example.gd

--- example.gd
+++ example.gd
@@ -17,8 +17,15 @@ func _ready() -> void:
 	demo_function(Vec2.value_lerp.bind(Vector2(-1.0, 1.0), 0.75), "Vec2")
 
 	demo_function(Arr.occurrences.bind([1, 2, 3, 2, 2, 4, 4, 4]), "Arr")
+	demo_function(Arr.sort_keys_by_values.bind({"a": 3, "b": 6, "c": 2, "d": 7}, false), "Arr")
+	demo_function(Arr.normalize_values.bind({"Sword": 1.5, "Trash": 3.0, "Treasure": 0.5}), "Arr")
 
+	demo_function(Random.random_angle, "Random")
+	demo_function(Random.random_direction, "Random")
+	demo_function(Random.pick_key_by_probabilities.bind({ "Sword": 0.3, "Trash": 0.6, "Treasure": 0.1 }, true), "Random")
 
+	demo_function(Strings.get_var_name.bind(self), "Strings")
+	
 func demo_function(f: Callable, static_class_name: String) -> void:
 	var args: String = ", ".join(f.get_bound_arguments().map(as_string))
 	args = with_rich_color(args, ARGS_COLOR)
