b321f26bb7003bae2c30068473f2bad039d929c8
whiteshampoo <benedikt.wicklein@gmail.com>
2025-02-20 10:00:04 +0100

    Create change_scene_button_simple.gd

---
+++ node/scene_change/change_scene_button_simple.gd
@@ -0,0 +1,19 @@
+class_name ChangeSceneButtonSimple
+extends Button
+## A self-managing [Button] to change the main-scene
+
+
+@export_file("*.tscn", "*.scn") var scene: String = ""
+
+
+func _ready() -> void:
+	if not FileAccess.file_exists(scene):
+		push_warning("File '%s' does not exist" % scene)
+	pressed.connect(_pressed)
+
+
+func _pressed() -> void:
+	if not FileAccess.file_exists(scene):
+		push_error("File '%s' does not exist" % scene)
+		return
+	get_tree().change_scene_to_file(scene)
